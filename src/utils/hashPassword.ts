9304ce072ec153b6339e3ccc1c089bd08ede3b3e
João Vitor <joaovitor3592@outlook.com>
2023-06-08 18:16:46 -0300

    feature: make passwordHash an abstract class

--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import { Api406Error } from './errors/api406Error';
 
 export const hashPassword = (password: string) => {
   const salt = bcrypt.genSaltSync(12);
@@ -11,3 +12,20 @@ export const hashPassword = (password: string) => {
 
   return hash;
 };
+
+export class PasswordHash {
+  private static genereateSalt() {
+    return bcrypt.genSaltSync(12);
+  }
+
+  public static hashPassword(password: string) {
+    const salt = this.genereateSalt();
+    const hash = bcrypt.hashSync(password, salt);
+
+    if (!hash) {
+      throw new Api406Error('An error has ocurred. Please try again later');
+    }
+
+    return hash;
+  }
+}
