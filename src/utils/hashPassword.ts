edff3ef5c4c21ea5d3b3f6e2a355892ba8367aa3
João Vitor <joaovitor3592@outlook.com>
2023-06-08 18:16:55 -0300

    chore: remove unused code

--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,18 +1,6 @@
 import bcrypt from 'bcryptjs';
 import { Api406Error } from './errors/api406Error';
 
-export const hashPassword = (password: string) => {
-  const salt = bcrypt.genSaltSync(12);
-  const hash = bcrypt.hashSync(password, salt);
-
-  if (!hash) {
-    console.error('Error hashing password');
-    return null;
-  }
-
-  return hash;
-};
-
 export class PasswordHash {
   private static genereateSalt() {
     return bcrypt.genSaltSync(12);
