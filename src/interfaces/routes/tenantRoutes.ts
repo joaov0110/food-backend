072972ece6a42d22fee9c8d62cd5ef05a5a4e3d4
João Vitor <joaovitor3592@outlook.com>
2023-07-01 05:10:46 -0300

    feature: add createTenant endpoint

--- src/interfaces/routes/tenantRoutes.ts
+++ src/interfaces/routes/tenantRoutes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+
 import { ITenantController } from '../controllers/tenantController';
+
+import createTenant from '../../utils/validations/createTenantValidator';
 
 export class TenantRoutes {
   public router: Router;
@@ -15,7 +18,11 @@ export class TenantRoutes {
   private routes() {
     this.router.get('/tenant', this.tenantController.getTenant);
 
-    this.router.post('/tenant', this.tenantController.createTenant);
+    this.router.post(
+      '/tenant',
+      createTenant,
+      this.tenantController.createTenant,
+    );
 
     this.router.put('/tenant', this.tenantController.updateTenant);
 
