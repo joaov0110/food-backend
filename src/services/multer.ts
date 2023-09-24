2a2b09fa3f956a4c81d48b071e613b39cb8b608c
João Vitor <joaovitor3592@outlook.com>
2023-09-24 15:05:44 -0300

    feature: add file filter for multer

--- src/services/multer.ts
+++ src/services/multer.ts
@@ -1,5 +1,24 @@
+import { Request } from 'express';
+
 import multer from 'multer';
+import { Api400Error } from '../utils/errors/api400Error';
 
 const storage = multer.memoryStorage();
 
-export const uploader = multer({ storage: storage });
+export const uploader = multer({
+  storage: storage,
+  fileFilter: (req: Request, file, cb) => {
+    const allowerMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif',
+    ];
+
+    if (allowerMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Api400Error('Invalid file type'));
+    }
+  },
+});
