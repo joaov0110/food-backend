c62120bcb081523cce7735b9432fd55186ddd465
João Vitor <joaovitor3592@outlook.com>
2023-10-02 04:27:48 -0300

    feature: add timestamps for catalog

---
+++ prisma/migrations/20231001231645_add_created_at_field_to_catalog/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `Catalog` ADD COLUMN `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE `Tenant` MODIFY `is_new_tenant` BIT(1) NOT NULL DEFAULT true;
